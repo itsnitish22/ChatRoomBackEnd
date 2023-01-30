d6dd2c028ea9b51765fb7f9b908351866db77f4e
Nitish Sharma <nitish.sharma1186@gmail.com>
2023-01-30 20:55:02 +0530

    room created, handled join room

--- socketserver.js
+++ socketserver.js
@@ -5,14 +5,30 @@ const { Server } = require("socket.io"); //import socketio
 const io = new Server(server); //creating io server
 const PORT = process.env.PORT || 3000 //port if env.PORT is available otherwise 3000
 
+var usernames = {} //maintaining usernames
+var activeRooms = [] //maintaining active rooms
+
 //on new connections
 io.on('connection', (socket) => {
 
     //broadcasting msg except itself
-    socket.on('chat message', (msg) => {
-        socket.broadcast.emit('chat message', msg);
+    socket.on('chat-message', (msg) => {
+        socket.broadcast.to(data.roomid).emit('chat-message', msg);
     });
 
+    //create room event
+    socket.on('create-room', (roomid) => {
+        socket.rooms = roomid //create a room in socketio
+        activeRooms.push(roomid) // add the room to activeRooms to maintain it
+    })
+
+    //when joining a room
+    socket.on('join-room', (data) => {
+        if (activeRooms.includes(data.roomid)) { //chec if the room exist
+            socket.join(data.roomid) //join the room
+            socket.broadcast.to(data.roomid).emit('update-about-room', data.username + ' has connected to this room'); //broadcast msg to that room about the joining of new user
+        }
+    })
 });
 
 //server start
